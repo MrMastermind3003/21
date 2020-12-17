fff47385613311e792ffbdcfab79eddf56d0003f
MrMastermind <75117071+MrMastermind3003@users.noreply.github.com>
2020-12-17 13:15:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,78 @@
-var wall,thickness;
-var bullet,speed,weight;
-var damage;
+var wall, thickness;
+//created a variable for wall and thickness
+
+  var bullet, speed, weight;
+//created a variable for bullet, speed and weight
+
 
 function setup() {
-  createCanvas(1600,400)
-  background(0,0,0); 
-  speed=Math.round(random(45,90));
-  thickness=Math.round(random(28,83));
-  wall = createSprite(1200,200,thickness,height/2);
-  bullet = createSprite(50,200,50,10);
-}
-
-function draw() {
+  createCanvas(1600,400);//created the canvas
+    
+    speed=random(223,321);
+    weight=random(30,52);
+    thickness=random(22,83);
+  //assigned random values to speed, weight and thickness
+    
+    bullet =createSprite(50,200,50,10);
+  //created the sprite for bullet
   
-  
-  bullet.shapeColor = "white";
-  buller.velocityX = speed;
-
-  if (hasCollided(bulllet,wall)){
-    bullet.velocity = 0;
-    damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
-    if(damage>10){
-      wall.shapeColor = color(255,0,0);
-    }
-    if(damage<10){
-      wall.shapeColor = color(0,255,0);
-    }
+    wall=createSprite(1200,200,thickness,height/2);
+    wall.shapeColor=color(80,80,80);
+  //created the sprite for wall and assigned a color to it
   }
 
-  drawSprites();
+  
+  function draw() {
+    background(255,255,255);//background
+      
+      bullet.velocityX=speed;
+    //Assigned the velocityX property of the bullet sprite to the speed variable
+
+
+    if(wall.x-bullet.x<(bullet.width+wall.width)/2){
+      bullet.velocityX=0;
+    //assigned velocity of the bullet to zero 
+      
+      var deformation =0.5*weight*speed*speed/22509;
+    //created the variable deformation
+      
+    if (deformation>180){
+      bullet.shapeColor=color(255,0,0);
+      }
+      
+    if (deformation<180 && deformation>100){
+      bullet.shapeColor=color(230,230,0);
+      }
+      
+    if(deformation<100){
+      bullet.shapeColor=color(0,255,0); 
+      }
+    //to change the color of the bullet according to the magnitude of deformation
+    }
+
+    if(hasCollided(bullet,wall)){
+      bullet.velocityX=0;
+    //assigned velocity zero to the bullet
+        
+      var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    //created the variable damage
+        
+    if (damage>10){
+        wall.shapeColor=color(255,0,0);
+      }
+        
+    if (damage<10){
+        wall.shapeColor=color(0,255,0);
+      }//to change the color of the wall according to the magnitude of damage
+      }
+drawSprites();
 }
-function hasCollided(object1,object2){
-  if(object2.x - object1.x<object1.width/2+object2.width/2){
-    return true;
-  } else{
-    return false
-  }
-}
+
+function  hasCollided(bullet1, wall1){
+      bulletRightEdge=bullet1.x +bullet1.width;
+      wallLeftEdge=wall1.x;
+  if (bulletRightEdge>=wallLeftEdge){
+      return true;
+    }
+    return false;
+  }//created function for has collided
